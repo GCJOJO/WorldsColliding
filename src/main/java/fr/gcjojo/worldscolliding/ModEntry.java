4c570291eb1bded7a9a58431272839226777b721
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2026-05-19 16:58:36 +0200

    Initial Commit

---
+++ src/main/java/fr/gcjojo/worldscolliding/ModEntry.java
@@ -0,0 +1,103 @@
+package fr.gcjojo.worldscolliding;
+
+import com.mojang.logging.LogUtils;
+import fr.gcjojo.worldscolliding.worldgen.dimension.ModDimensions;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ModEntry.MODID)
+public class ModEntry
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "worldscolliding";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ModEntry(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    public static Logger getLogger() { return LOGGER; }
+
+    @SubscribeEvent
+    public void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event)
+    {
+        LOGGER.info("Player {} changed dimension {}", event.getEntity().getName().getString(), event.getTo().toString());
+        //event.getEntity().changeDimension()
+        if(event.getTo() == ModDimensions.STORY_DIM_LEVEL_KEY)
+        {
+            LOGGER.info("Player {} joined STORY Dimension", event.getEntity().getName().getString());
+            event.getEntity().teleportTo(100, 100, 100);
+            CompoundTag additionalSaveData = new CompoundTag();
+            event.getEntity().readAdditionalSaveData(additionalSaveData);
+        }
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
---
+++ src/main/java/fr/gcjojo/worldscolliding/worldgen/dimension/ModDimensions.java
@@ -0,0 +1,75 @@
+package fr.gcjojo.worldscolliding.worldgen.dimension;
+
+import fr.gcjojo.worldscolliding.ModEntry;
+import io.netty.util.Constant;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.chunk.ChunkGenerators;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalLong;
+
+public class ModDimensions
+{
+    public static final ResourceKey<LevelStem> STORY_DIM_KEY = ResourceKey.create(Registries.LEVEL_STEM,
+            new ResourceLocation(ModEntry.MODID, "story"));
+    public static final ResourceKey<Level> STORY_DIM_LEVEL_KEY = ResourceKey.create(Registries.DIMENSION,
+            new ResourceLocation(ModEntry.MODID, "story"));
+    public static final ResourceKey<DimensionType> STORY_DIM_TYPE = ResourceKey.create(Registries.DIMENSION_TYPE,
+            new ResourceLocation(ModEntry.MODID, "story_type"));
+
+
+    public static void bootstrapType(BootstapContext<DimensionType> context)
+    {
+        context.register(STORY_DIM_TYPE, new DimensionType(
+                OptionalLong.of(12000),
+                false,
+                false,
+                false,
+                false,
+                1.0,
+                true,
+                true,
+                0,
+                256,
+                256,
+                BlockTags.INFINIBURN_OVERWORLD,
+                BuiltinDimensionTypes.OVERWORLD_EFFECTS,
+                1.0f,
+                new DimensionType.MonsterSettings(false, false, ConstantInt.of(0), 0)
+        ));
+    }
+
+    public static void bootstrapStem(BootstapContext<LevelStem> context)
+    {
+        HolderGetter<Biome> biomeRegistry = context.lookup(Registries.BIOME);
+        HolderGetter<DimensionType> dimTypes = context.lookup(Registries.DIMENSION_TYPE);
+        HolderGetter<NoiseGeneratorSettings> noiseGenSettings = context.lookup(Registries.NOISE_SETTINGS);
+
+        FlatLevelSource chunkGenerator = new FlatLevelSource(
+                new FlatLevelGeneratorSettings(Optional.empty(), biomeRegistry.getOrThrow(Biomes.PLAINS), new ArrayList<Holder<PlacedFeature>>()));
+
+        LevelStem stem = new LevelStem(dimTypes.getOrThrow(ModDimensions.STORY_DIM_TYPE), chunkGenerator);
+        context.register(STORY_DIM_KEY, stem);
+    }
+}
